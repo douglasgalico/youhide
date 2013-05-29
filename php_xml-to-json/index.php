9d56ad2ce094fdab7adab6d5666ad82af32ce5b3
Youri hideOut <youri@youhide.com.br>
2013-05-29 15:50:52 -0300

    90%

--- php_xml-to-json/index.php
+++ php_xml-to-json/index.php
@@ -1,36 +1,31 @@
 <?php   
 
+date_default_timezone_set("America/Sao_Paulo");
+
 $param = ($_GET) ? $_GET['data_inicial'] : '';
 $xml = simplexml_load_file("horoscopo.xml");
-foreach($xml->signos as $signo){
-	foreach ($signo as $dados_signo){
-		$i = 0;
-		$nome_signo = (string) $dados_signo->attributes()->nome;
-		foreach ($dados_signo->dias->dia as $info_previsao){	
-			$arrPrevisoes['previsoes'][$i] = array();
-			$arrPrevisoes['previsoes'][$i]['signo'] = $nome_signo;
-			$arrPrevisoes['previsoes'][$i]['previsao'] = (string) $info_previsao;
-			$arrPrevisoes['previsoes'][$i]['data'] = (string) $info_previsao['data'];
-			$i++;
-		}
-	}
-}
+$paramEx = explode("/", $param);
+$paramFormat = $paramEx[2]."-".$paramEx[1]."-".$paramEx[0];
+$dataInicial = strtotime($paramFormat);
+$dataFinal = strtotime($paramFormat."+30 days");
+
 foreach($xml->signos as $signo){
 	for ($i = 0; $i < 12; $i++){
 		$nome_signo = $signo->signo[$i]->attributes()->nome."<br/>";
 		$cont_datas = count($signo->signo[$i]->dias->dia);
-		for ($j = 0; $j < $cont_datas; $j++){
-			if ($signo->signo[$i]->dias->dia[$j]['data'] == $param){
-				echo "SIGNO: ".$nome_signo;
-				echo "PREVISAO: ".$signo->signo[$i]->dias->dia[$j]."<br/>";
-				echo "DATA: ".$signo->signo[$i]->dias->dia[$j]['data']."<br/><br/>";
-				$arrPrev['previsoes'][$i] = array("signo" => $nome_signo
+		for ($j = 0; $j < $cont_datas; $j++){	
+			$dataSigno = explode("/", $signo->signo[$i]->dias->dia[$j]['data']);
+			$dataSigno = strtotime($dataSigno[2]."-".$dataSigno[1]."-".$dataSigno[0]);
+			if($dataSigno >= $dataInicial && $dataSigno <= $dataFinal){
+				$arrPrev['previsoes'][] = array("signo" => $nome_signo
 												 , "previsao" => (string) $signo->signo[$i]->dias->dia[$j]
 												 , "data" => (string) $signo->signo[$i]->dias->dia[$j]['data']);
-			}
+			}									 
 		}
 	}
 }
-echo json_encode($arrPrevisoes);
+
+//echo "<pre>".print_r($arrPrev, true)."</pre>";
+echo json_encode($arrPrev);
 
 ?>
